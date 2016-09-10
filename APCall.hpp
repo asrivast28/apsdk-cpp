b955dc069a6209dd015643e9ca2c71e80ade70c4
Ankit Srivastava <asrivast@gatech.edu>
2016-09-09 20:08:06 -0400

    Added missing file comment.

--- APCall.hpp
+++ APCall.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file APCall.hpp
+ * @brief Functionality for error checking calls to C API.
+ * @author Ankit Srivastava <asrivast@gatech.edu>
+ * @version 1.0
+ * @date 2016-09-09
+ */
 #ifndef APSDK_APCALL_HPP_
 #define APSDK_APCALL_HPP_
 
